241243c8258821a99f10e845d26e0628d5d4efaa
ryan erswell <ersie93@googlemail.com>
2021-02-17 15:11:20 +0000

    restaurants and reviews app

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,4 @@ class Review < ApplicationRecord
 	belongs_to :restaurant
 	validates :content, presence: true
 	validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5]}, numericality: { only_integer: true }
-	# validates :rating, numericality: { only_integer: true }
 end
